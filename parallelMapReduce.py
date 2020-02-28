7dbcf98b6c6ca437e10a6bd8bef9d07ee2c2938f
dgarciaher <dgarciaher@miners.utep.edu>
2020-02-28 16:41:44 -0700

    updating code

--- parallelMapReduce.py
+++ parallelMapReduce.py
@@ -12,7 +12,7 @@ file7 = open("shakespeare7.txt", "r", encoding="utf8")
 file8 = open("shakespeare8.txt", "r", encoding="utf8")
 
 files = [file1, file2, file3, file4, file5, file6, file7, file8]  # putting all files in a list for easy access
-
+p_list = pymp.shared.array(files)
 
 word_dictionary = {
 					"hate": 0,
@@ -33,23 +33,47 @@ word_dictionary = {
 					"honest": 0
 				}
 
-start = time.time()
-for f in files:
+# p_dictionary = pymp.shared.dict()
+
+thread_num = 2
+
+print("\n")
+print("threads used:", thread_num , "\n\n")
+
+totalTimeStart = time.time()
+
+with pymp.Parallel(thread_num) as p1:
+
+	# lock = p1.lock()
+	
+	for i in p1.range(len(p_list)):
+
+		fTimeStart = time.time()
+		f = files[i]
+
 		for line in f:
+
 			line = line.strip()
 			line = line.lower()
 			line = line.strip(".,';()")  # removes special characters that might be attached to the word
-			# line = line.strip(['.', ',', '\'', ';', '(', ')'])
-
 			wLine = line.split(" ")  # getting all words in every line separated by a space
 
 			for w in wLine:
-				if w in word_dictionary:
-					word_dictionary[w] += 1
+				with p1.lock:
 
-stop = time.time()
-print("time for non-parallel map reduce: ", (stop-start) , "\n")
-# print("threads used:", thread_num)
+					if w in word_dictionary:
+						word_dictionary[w] += 1
 
+		fTimeStop = time.time()
+		p1.print("time for file no.", i+1, ": ",(fTimeStop-fTimeStart) , "\n")
+
+totalTimeStop = time.time()
+print("\n")
+print("total time of parallel program", (totalTimeStop-totalTimeStart), "\n\n")
+
+
+print("--------------word dictionary--------------")
 for i in list(word_dictionary.keys()):
 	print(i, ": ", word_dictionary[i])
+
+print("-------------------------------------------")
